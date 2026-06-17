f95a01289453506c5d7bc72bf6e1b3b099783e24
Kkiran2003 <kapurkarkiran123@gmail.com>
2026-06-17 21:11:14 +0530

    add car schema validation  and model and insert data

---
+++ car.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+main().then(res =>
+    console.log("connection succesfully"))
+    .catch(err =>
+        console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/Automobile');
+
+}
+
+const carSchema = mongoose.Schema({
+    model: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String
+    },
+    price: {
+        type: Number
+    }
+})
+
+const Car = mongoose.model("Car", carSchema);
+
+let car1 = new Car({
+    model : "BMW 3 Series",
+    brand: "BMW",
+    price: "55678944"
+})
+
+car1.save().then(res =>{
+    console.log(res)
+}).catch(err =>{
+    console.log(err)
+})
